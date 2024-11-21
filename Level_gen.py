62d2bdf4228e039560e6339178195e9bf72bd6db
PlagiDocy <46785308+P1agueDoc@users.noreply.github.com>
2024-11-21 21:44:25 +0500

    Update Level_gen.py
    Level gen

--- Level_gen.py
+++ Level_gen.py
@@ -4,6 +4,10 @@ import random
 class Map:
     safe = "🌫 " #:fog:
     danger = "🔥 "
+    trap_room ="❄️ "
+    random_stuff = "🎲 "
+    trader_room ="🧮 "
+    no_way = "🏔 "
 
     def __init__(self, height, length):
         self.height = height
@@ -13,10 +17,21 @@ class Map:
     def map_gen(self):
         for i in range(1, self.height + 1):
             for b in range(1, self.length + 1):
-                level_id = random.randint(0, 1)
+                level_id = random.randint(0, 4)
                 if level_id == 0:
                     self.level_row_data += str(self.safe)
                 elif level_id == 1:
                     self.level_row_data += str(self.danger)
+                elif level_id == 2:
+                    self.level_row_data += str(self.trap_room)
+                elif level_id == 3:
+                    self.level_row_data += str(self.random_stuff)
+                elif level_id == 4:
+                    a = random.randint(0, 2)
+                    if a == 0:
+                        self.level_row_data += str(self.trader_room)
+                    else:
+                        self.level_row_data += str(self.no_way)
+
             self.level_row_data += "\n"
         return self.level_row_data
